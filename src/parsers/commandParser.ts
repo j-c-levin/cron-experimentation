c2a91b2676dbee1e6bd1b122aa4ea100c8393319
Joshua Levin <joshua.c.levin@gmail.com>
2018-06-23 13:24:12 +0100

    small

--- src/parsers/commandParser.ts
+++ src/parsers/commandParser.ts
@@ -1,18 +1,18 @@
 export class CommandParser {
-    command: string;
+    private command: string;
 
     constructor(input: string[], commandIndex: number) {
         this.command = this.parseCommand(input, commandIndex);
     }
 
     // Construct the cron command by concatenating all the arguments after the day of the week command
-    parseCommand(input: string[], commandIndex: number): string {
+    public parseCommand(input: string[], commandIndex: number): string {
         return input.reduce((previous, current, index) => {
             return (index < commandIndex) ? previous : `${previous} ${current}`;
         }, '');
     }
 
-    printCommand(): void {
+    public printCommand(): void {
         console.log('command     ', this.command);
     }
 }
