300158adac62ea4f8ba29eaf2cf7393f16a56e4b
Joshua Levin <joshua.c.levin@gmail.com>
2018-06-24 17:06:19 +0100

    day of month parser tests

--- src/parsers/dayOfMonthParser.ts
+++ src/parsers/dayOfMonthParser.ts
@@ -5,26 +5,31 @@ import { NumberMatcher } from '../matchers/numberMatcher';
 import { RangeMatcher } from '../matchers/rangeMatcher';
 
 export class DayOfMonthParser implements Parser {
-    properties: MatcherProperties = {
+    public name = 'day of month ';
+    private properties: MatcherProperties = {
         minValue: 1,
-        maxValue: 31
-    }
-    name = 'day of month ';
-    value: Matcher;
-    children: Parser[] = [];
+        maxValue: 31,
+    };
+    private value: Matcher;
+    private children: Parser[] = [];
 
     constructor(input: string) {
         this.value = this.splitDataString(input);
     }
 
-    splitDataString(input: string): Matcher {
+    // Match either in this object or in any child objects
+    public match(input: number): boolean {
+        return this.value.match(input) || this.children.some((child) => child.match(input));
+    }
+
+    private splitDataString(input: string): Matcher {
 
         // Input is a list, must check this first for recursion to work
         if (input.includes(',')) {
             // Split into elements
             const list = input.split(',');
             // Create new Parsers recursively with the individual elements
-            list.forEach(element => {
+            list.forEach((element) => {
                 this.children.push(new DayOfMonthParser(element));
             });
             // Matching will be handled by the children
@@ -51,10 +56,5 @@ export class DayOfMonthParser implements Parser {
 
         // Input matches no known type, throw error
         throw new Error(`Input ${input} as a day of month does not match any known type`);
-    }
-
-    // Match either in this object or in any child objects
-    match(input: number): boolean {
-        return this.value.match(input) || this.children.some(child => child.match(input));
     }
 }
